8e693631aa4d850e2cd7bdb83615b64a7c9c0b71
Russel Bulanon <rbulanon@exist.com>
2017-04-14 15:01:51 +0800

    Added SchoolController

---
+++ src/main/java/com/sanmateo/controller/SchoolController.java
@@ -0,0 +1,14 @@
+package com.sanmateo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by rsbulanon on 4/14/17.
+ */
+@Controller
+@RequestMapping(path = "/api")
+public class SchoolController {
+
+    
+}
