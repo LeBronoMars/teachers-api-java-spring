0ccc26a70343adf961b50540ac48923c7676611d
Russel Bulanon <rbulanon@exist.com>
2017-04-14 15:01:16 +0800

    Added UsernameOrEmailAlreadyExistException in BaseService

--- src/main/java/com/sanmateo/service/BaseService.java
+++ src/main/java/com/sanmateo/service/BaseService.java
@@ -4,7 +4,7 @@ package com.sanmateo.service;
 import com.sanmateo.model.BaseModel;
 
 /**
- * Created by ginduc on 20/07/16.
+ * Created by rsbulanon on 4/14/17.
  */
 abstract public class BaseService {
     class NotFoundException extends RuntimeException {
@@ -13,4 +13,9 @@ abstract public class BaseService {
         }
     }
 
+    class UsernameOrEmailAlreadyExistException extends RuntimeException {
+        public UsernameOrEmailAlreadyExistException(String usernameOrEmail) {
+            super(usernameOrEmail + " already in used.");
+        }
+    }
 }
