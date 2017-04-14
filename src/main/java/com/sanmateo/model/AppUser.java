11dfe2fbbb63de277a9f5fad313f8205fee9efa6
Russel Bulanon <rbulanon@exist.com>
2017-04-15 06:14:38 +0800

    Set example value for each field member of Models using @ ApiModelProperty annotation

--- src/main/java/com/sanmateo/model/AppUser.java
+++ src/main/java/com/sanmateo/model/AppUser.java
@@ -3,6 +3,7 @@ package com.sanmateo.model;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.annotations.ApiModelProperty;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -19,44 +20,55 @@ public class AppUser extends BaseModel {
 
     @Column(unique = true, nullable = false)
     @JsonProperty("employee_no")
+    @ApiModelProperty(example = "EMP-00001")
     private String employeeNo;
 
     @Column(nullable = false)
     @JsonProperty("first_name")
+    @ApiModelProperty(example = "Ned")
     private String firstName;
 
     @Column(nullable = false)
     @JsonProperty("last_name")
+    @ApiModelProperty(example = "Johnson")
     private String lastName;
 
     @Column(nullable = false)
     @JsonProperty("middle_name")
+    @ApiModelProperty(example = "Flanders")
     private String middleName;
 
     @Column(nullable = false)
     @JsonProperty("address")
+    @ApiModelProperty(example = "Hagonoy, Bulacan")
     private String address;
 
     @Column(nullable = false)
     @JsonProperty("contact_no")
+    @ApiModelProperty(example = "09123456789")
     private String contactNo;
 
     @Temporal(TemporalType.TIMESTAMP)
     @JsonProperty("birth_date")
     @Column(name = "birth_date", nullable = false)
     @JsonFormat(pattern="yyyy-MM-dd")
+    @ApiModelProperty(example = "1988-09-25")
     private Date birthDate;
 
     @Column(unique = true, nullable = false)
+    @ApiModelProperty(example = "ned@flanders.com")
     private String email;
 
     @Column(unique = true, nullable = false)
+    @ApiModelProperty(example = "nedflanders")
     private String username;
 
     @Column(columnDefinition = "CHAR(30)", length = 30, nullable = false)
+    @ApiModelProperty(example = "SUPER_ADMIN")
     private String role;
 
     @Column(nullable = false)
+    @ApiModelProperty(example = "P@ssw0rd")
     private String password;
 
     @Column(columnDefinition = "CHAR(10)", length = 10, nullable = false)
@@ -68,15 +80,18 @@ public class AppUser extends BaseModel {
 
     @Column(columnDefinition = "CHAR(50)", length = 50)
     @NotNull(message = "position is required.")
+    @ApiModelProperty(example = "SUPER_ADMIN")
     private String position;
 
     @Column(columnDefinition = "CHAR(6)", length = 6)
     @NotNull(message = "gender is required.")
+    @ApiModelProperty(example = "Male")
     private String gender;
 
     @Column(columnDefinition = "CHAR(10)", length = 10)
     @NotNull(message = "civil status is required.")
     @JsonProperty("civil_status")
+    @ApiModelProperty(example = "Single")
     private String civilStatus;
 
     public String getEmployeeNo() {
--- src/main/java/com/sanmateo/model/BaseModel.java
+++ src/main/java/com/sanmateo/model/BaseModel.java
@@ -1,7 +1,10 @@
 package com.sanmateo.model;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 import org.hibernate.annotations.GenericGenerator;
 
@@ -11,7 +14,7 @@ import java.util.Date;
 
 /**
  * Base implementation of model classes
- *
+ * 
  * Created by rsbulanon on 4/14/17.
  */
 @Data
@@ -21,34 +24,41 @@ public abstract class BaseModel implements Serializable {
     @GeneratedValue(generator = "uuid2")
     @GenericGenerator(name = "uuid2", strategy = "uuid2")
     @Column(columnDefinition = "CHAR(36)", length = 36)
+    @ApiModelProperty(readOnly = true)
     protected String id;
 
     @Temporal(TemporalType.TIMESTAMP)
     @Column(name = "created", nullable = false)
-    @JsonFormat(pattern="yyyy-MM-dd'T'HH:mm:ss'Z'")
-    private Date created;
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'")
+    @ApiModelProperty(readOnly = true)
+    @JsonProperty("created_at")
+    private Date createdAt;
 
     @Temporal(TemporalType.TIMESTAMP)
-    @JsonFormat(pattern="yyyy-MM-dd'T'HH:mm:ss'Z'")
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'")
     @Column(name = "updated", nullable = false)
-    private Date updated;
+    @ApiModelProperty(readOnly = true)
+    @JsonProperty("updated_at")
+    private Date updatedAt;
 
     @Column
+    @ApiModelProperty(readOnly = true, example = "true")
     private Boolean active;
 
     @Column
     @JsonProperty("is_synced")
+    @ApiModelProperty(example = "true")
     private boolean isSynced;
 
     @PrePersist
     protected void onCreate() {
-        updated = created = new Date();
+        updatedAt = createdAt = new Date();
         active = true;
         isSynced = true;
     }
 
     @PreUpdate
     protected void onUpdate() {
-        updated = new Date();
+        updatedAt = new Date();
     }
 }
